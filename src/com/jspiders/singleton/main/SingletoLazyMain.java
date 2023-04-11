cca5810f6d2150e115c86fbd22bcab258c9b4b35
bharat <ladebharat9@gmail.com>
2023-04-11 19:15:24 +0530

    first commit

---
+++ src/com/jspiders/singleton/main/SingletoLazyMain.java
@@ -0,0 +1,15 @@
+package com.jspiders.singleton.main;
+
+import com.jspiders.singleton.object.SingletonLazy;
+
+public class SingletoLazyMain {
+public static void main(String[] args) {
+	SingletonLazy object1=SingletonLazy.getObject();
+	SingletonLazy object2=SingletonLazy.getObject();
+	SingletonLazy object3=SingletonLazy.getObject();
+	
+	System.out.println(object1);
+	System.out.println(object2);
+	System.out.println(object3);
+}
+}
